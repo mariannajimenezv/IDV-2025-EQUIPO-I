1e284714279e5bcddafcedb0f9935780163612bb
Samuelovich <samuelretamero@outlook.com>
2026-01-19 15:54:11 +0100

    Enemigo base con navmesh

--- Lumi's Adventure/Assets/Scripts/Enemy.cs
+++ Lumi's Adventure/Assets/Scripts/Enemy.cs
@@ -1,20 +1,109 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [Header("Configuración")]
-    public float speed = 3f;
-    public int damage = 20;
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask Ground, Player;
+    
+    
+    [Header("Estadisticas del enemigo")]
+    
+    public int health = 50;
+    public float sightRange; 
+    public float attackRange;
+    public float attackCooldown;
+
+    //public float speed = 3f;
+    public int damage = 1;
+    public bool hasAttacked;
 
     [Header("Ruta de Patrulla")]
-    public Transform[] patrolPoints;
-    private int currentPointIndex = 0;
+    //public Transform[] patrolPoints;
+    //private int currentPointIndex = 0;
+    public Vector3 patrolPoint;
+    bool patrolPointSet;
+    public float patrolPointRange;
 
-    void Update()
+    public bool inSightRange, inAttackRange;
+
+
+    private void Awake()
     {
-        Patrol();
+        player = GameObject.Find("Lumi").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
+    private void Update()
+    {
+        //Definición del rango de vision y ataque del enemigo
+        inSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
+        inAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
+
+        if (!inSightRange && !inAttackRange) IdlePatrol();
+        if (inSightRange && !inAttackRange) ChasingPlayer();
+        if (inSightRange && inAttackRange) Attack();
+
+        //OnDrawGizmosSelected();
+        //Patrol();
+    }
+
+
+    private void IdlePatrol()
+    {
+        if (!patrolPointSet) SetPatrolPoint();
+
+        if(patrolPointSet) agent.SetDestination(patrolPoint);
+
+        Vector3 patrolPointDistance = transform.position - patrolPoint;
+
+        if(patrolPointDistance.magnitude < 1f) patrolPointSet = false;
+    }
+    private void SetPatrolPoint()
+    {
+        float randomZ = Random.Range(-patrolPointRange, patrolPointRange);
+        float randomX = Random.Range(-patrolPointRange, patrolPointRange);
+
+        patrolPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        // Comprobación que el punto de destino este en el mapa
+        if(Physics.Raycast(patrolPoint, -transform.up, 2f, Ground)) patrolPointSet = true;
+    }
+
+    private void ChasingPlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void Attack()
+    {
+        // 2. Lógica de Daño (Hitbox)
+        if(inAttackRange)
+        {
+            Debug.Log("Enemigo golpea a Lumi ");
+            //LumiController.TakeDamage();
+        }
+        
+        hasAttacked = true;
+        Invoke(nameof(ResetAttack), attackCooldown);
+    }
+    private void ResetAttack()
+    {
+        hasAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    /*
     void Patrol()
     {
         if (patrolPoints.Length == 0) return;
@@ -29,7 +118,7 @@ public class Enemy : MonoBehaviour
         {
             currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length;
         }
-    }
+    }*/
 
     private void OnCollisionEnter(Collision collision)
     {

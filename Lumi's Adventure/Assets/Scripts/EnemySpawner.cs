4e7302d4a44b1566fe6db243a48e77a13230ce14
Samuelovich <samuelretamero@outlook.com>
2026-01-20 17:32:19 +0100

    Enemy spawners creados

--- Lumi's Adventure/Assets/Scripts/EnemySpawner.cs
+++ Lumi's Adventure/Assets/Scripts/EnemySpawner.cs
@@ -12,15 +12,19 @@ public class EnemySpawner : MonoBehaviour
 
     private void Awake()
     {
-        enemyType = Random.Range(0, 1); // 0: Mushroom, 1: Cactus
+        enemyType = Random.Range(0, 2); // 0: Mushroom, 1: Cactus
 
         if (enemyType == 0)
         {
             Instantiate(mushroomPrefab, transform.position, Quaternion.identity);
+            Debug.Log("Seta creada");
+
         }
         else
         {
             Instantiate(cactusPrefab, transform.position, Quaternion.identity);
+            Debug.Log("Cactus creado");
+
         }
     }
 

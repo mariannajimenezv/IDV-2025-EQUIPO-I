cfb03aa9cc142fd9219973c842c821beab9e2bef
mariannajimenezv <127933982+mariannajimenezv@users.noreply.github.com>
2026-01-20 20:32:41 +0100

    Revert "Mensaje fragmentos y errata de controles"
    This reverts commit 395380ae27bae0217990b19bc4cb6032649e36cf.

--- Lumi's Adventure/Assets/Scripts/Observer/HUDController.cs
+++ Lumi's Adventure/Assets/Scripts/Observer/HUDController.cs
@@ -11,8 +11,6 @@ public class HUDController : MonoBehaviour, ILumiObserver
     public TMP_Text livesText;      
     public TMP_Text fragmentsText;  
     public TMP_Text powerUpMsg;    
-    public TMP_Text fragmentMsg;
-    public GameObject fragmentMsgImage;
 
     [Header("UI Elements")]
     public Image lifeBar;    
@@ -51,13 +49,7 @@ public class HUDController : MonoBehaviour, ILumiObserver
     public void OnFragmentCount(int value)
     {
         if (fragmentsText != null)
-            fragmentsText.text = "Fragments: " + value;
-        
-        if(fragmentMsg != null && value == 10)
-        {
-            fragmentMsg.text = "You got all the fragments! Search for the exit to complete the level";
-            fragmentMsgImage.SetActive(true);
-        }
+            fragmentsText.text = "Fragmentos: " + value;
     }
 
     public void OnPowerUp(string value)
